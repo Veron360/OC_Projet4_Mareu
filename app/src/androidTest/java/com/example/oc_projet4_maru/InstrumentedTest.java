4d779b7820bafc023b4db89f53323a52a0f6db7b
Veron360 <veron360.op@gmail.com>
2020-03-03 11:57:14 +0100

    mise à jour 03/03

--- app/src/androidTest/java/com/example/oc_projet4_maru/InstrumentedTest.java
+++ app/src/androidTest/java/com/example/oc_projet4_maru/InstrumentedTest.java
@@ -1,5 +1,8 @@
 package com.example.oc_projet4_maru;
 
+import android.widget.DatePicker;
+
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
@@ -10,10 +13,14 @@ import com.example.oc_projet4_maru.UI.Controller.ListMeetingActivity;
 import com.example.oc_projet4_marumaru.R;
 
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
@@ -25,6 +32,7 @@ import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -54,7 +62,7 @@ public class InstrumentedTest {
      * Génere un seul meeting
      */
 
-    public void meeting(){
+    public void meeting(String sujet, int year, int month, int day, List<String> emails, int room){
 
 
         // clique sur bouton deuxiéme activiter
@@ -62,7 +70,7 @@ public class InstrumentedTest {
         // Vérifie su bien sur l'autre activiter
         onView(withId(R.id.EditTextSujet)).check(matches(isDisplayed()));
         // clique sur l'éditText et écrit "Sujet 1"
-        onView(withId(R.id.EditTextSujet)).perform(typeText("Sujet 1"));
+        onView(withId(R.id.EditTextSujet)).perform(typeText(sujet));
         //rentre le clavier
         onView(withId(R.id.EditTextMail)).perform(closeSoftKeyboard());
         // clique sur "Ajouter une Heure" + "ok"
@@ -70,20 +78,18 @@ public class InstrumentedTest {
         onView(withText("OK")).perform(click());
         // clique sur "Ajouter une date" + "ok"
         onView(withId(R.id.button_date)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(year,month, day));
         onView(withText("OK")).perform(click());
         //Spinner
         onView(withId(R.id.EditTextSalle)).perform(click());
-        onView(withText("Salle n°5")).perform(click());
+        onView(withText("Salle n°" + room)).perform(click());
         // clique sur l'éditText Mail (2 utilisateurs)
-        onView(withId(R.id.EditTextMail)).perform(typeText("toto@gmail.com"));
-        //rentre le clavier
-        onView(withId(R.id.EditTextMail)).perform(closeSoftKeyboard());
-        //Ajoute un chip-user
-        onView(withId(R.id.Button_add_chip)).perform(click());
-        onView(withId(R.id.EditTextMail)).perform(typeText("homer@gmail.com"));
-        //rentre le clavier
-        onView(withId(R.id.EditTextMail)).perform(closeSoftKeyboard());
-        onView(withId(R.id.Button_add_chip)).perform(click());
+        for (String email : emails){
+            onView(withId(R.id.EditTextMail)).perform(typeText(email));
+            //rentre le clavier
+            onView(withId(R.id.EditTextMail)).perform(closeSoftKeyboard());
+            onView(withId(R.id.Button_add_chip)).perform(click());
+        }
         // clique sur sauvegarder
         onView(withId(R.id.Saved)).perform(click());
 
@@ -104,9 +110,15 @@ public class InstrumentedTest {
         onView(withId(R.id.List_Meeting_RecycleurView))
                 .check(matches(hasMinimumChildCount(0)));
         //Ajoute meeting
-        meeting();
+        meeting("sujet1",2020,3, 3, Arrays.asList("toto@gmail.com", "homer@gmail.com"), 5);
         // verifie si la liste utilisateur contient bien 1 élement de plus
         onView(ViewMatchers.withId(R.id.List_Meeting_RecycleurView)).check(matches(hasMinimumChildCount(1)));
+        // Vérifier si le sujet est correct
+        onView(ViewMatchers.withId(R.id.Nom_Fragment)).check(matches(withText("sujet1")));
+        // Vérifier si les mails sont correct
+        onView(ViewMatchers.withId(R.id.Mail_Fragment)).check(matches(withText("toto@gmail.com, homer@gmail.com")));
+        // Vérifier si la salle est correct
+        onView(ViewMatchers.withId(R.id.Salle_reunion)).check(matches(withText("Salle n°5")));
 
 
     }
@@ -120,7 +132,7 @@ public class InstrumentedTest {
         onView(withId(R.id.List_Meeting_RecycleurView))
                 .check(matches(hasMinimumChildCount(0)));
         //Ajoute Meeting
-        meeting();
+        meeting("sujet1",2020,3, 3, Arrays.asList("toto@gmail.com", "homer@gmail.com"), 5);
         // supprime le 1er élément de la liste
         onView(withId(R.id.DeleteButton)).perform(click());
         // vérifier si la liste est vide de nouveau
@@ -136,42 +148,11 @@ public class InstrumentedTest {
     public void checkIfFilteringMeetingsIsWorking() {
 
         // Ajouter 1er meeting
-        meeting();
-        // Vérifier si le sujet est correct
-        onView(ViewMatchers.withId(R.id.Nom_Fragment)).check(matches(withText("Sujet 1")));
-        // Vérifier si les mails sont correct
-        onView(ViewMatchers.withId(R.id.Mail_Fragment)).check(matches(withText("toto@gmail.com, homer@gmail.com")));
-        // Vérifier si la salle est correct
-        onView(ViewMatchers.withId(R.id.Salle_reunion)).check(matches(withText("Salle n°5")));
+        meeting("sujet1",2020,3, 3, Arrays.asList("toto@gmail.com", "homer@gmail.com"), 5);
 
         // Ajouter  2éme meeting
+        meeting("sujet2",2019,2, 5, Arrays.asList("toto@gmail.com", "homer@gmail.com"), 3);
 
-        // clique sur bouton deuxiéme activiter
-        onView(withId(R.id.fab)).perform(click());
-        // Vérifie su bien sur l'autre activiter
-        onView(withId(R.id.EditTextSujet)).check(matches(isDisplayed()));
-        // clique sur l'éditText et écrit "Sujet 1"
-        onView(withId(R.id.EditTextSujet)).perform(typeText("Sujet 2"));
-        //rentre le clavier
-        onView(withId(R.id.EditTextMail)).perform(closeSoftKeyboard());
-        // clique sur "Ajouter une Heure" + "ok"
-        onView(withId(R.id.button_hour)).perform(click());
-        onView(withText("OK")).perform(click());
-        // clique sur "Ajouter une date" + "ok"
-        onView(withId(R.id.button_date)).perform(click());
-        onView(withText("OK")).perform(click());
-        //Spinner
-        onView(withId(R.id.EditTextSalle)).perform(click());
-        onView(withText("Salle n°2")).perform(click()); // change Salle
-        // clique sur l'éditText Mail (2 utilisateurs)
-        onView(withId(R.id.EditTextMail)).perform(typeText("toto@gmail.com"));
-        onView(withId(R.id.EditTextMail)).perform(closeSoftKeyboard());
-        onView(withId(R.id.Button_add_chip)).perform(click());
-        onView(withId(R.id.EditTextMail)).perform(typeText("homer@gmail.com"));
-        onView(withId(R.id.EditTextMail)).perform(closeSoftKeyboard());
-        onView(withId(R.id.Button_add_chip)).perform(click());
-        // clique sur sauvegarder
-        onView(withId(R.id.Saved)).perform(click());
         //vérifie sur la liste contient bien 2 meetings
         onView(withId(R.id.List_Meeting_RecycleurView))
                 .check(matches(hasChildCount(ITEMS_COUNT +2)));
@@ -184,18 +165,30 @@ public class InstrumentedTest {
         // clique sur le spinner Room
         onView(withId(R.id.room_spinner_dialog)).perform(click());
         onData(CoreMatchers.allOf(is(instanceOf(String.class)),
-               is("Salle n°2"))).inRoot(isPlatformPopup()).perform(click());
+                is("Salle n°5"))).inRoot(isPlatformPopup()).perform(click());
         //clique sur valider
         onView(withId(android.R.id.button1)).perform(click());
         //On vérifie qu'il reste un item dans la liste
         onView(withId(R.id.List_Meeting_RecycleurView))
                 .check(matches(hasChildCount(ITEMS_COUNT +1)));
         //vérifie si la room match
-        onView(ViewMatchers.withId(R.id.Salle_reunion)).check(matches(withText("Salle n°2")));
+        onView(ViewMatchers.withId(R.id.Salle_reunion)).check(matches(withText("Salle n°5")));
+
+        //Clique sur le filtre
+        onView(withId(R.id.dialog)).perform(click());
+        //Clique sur le spinner date
+        onView(withId(R.id.date_spinner_dialog)).perform(click());
+        onData(CoreMatchers.allOf(is(instanceOf(String.class)),
+                is("3/3/2020"))).inRoot(isPlatformPopup()).perform(click());
+        //clique sur valider
+        onView(withId(android.R.id.button1)).perform(click());
+        //On vérifie qu'il reste un item dans la liste
+        onView(withId(R.id.List_Meeting_RecycleurView))
+                .check(matches(hasChildCount(ITEMS_COUNT +1)));
+        //vérifie si la room match
+        onView(ViewMatchers.withId(R.id.date_Fragment)).check(matches(withText("3/3/2020")));
+
+
 
     }
-
-
-
-
 }
